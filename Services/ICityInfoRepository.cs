2f3fc0196daca8c245fee9dffc0b52c814c4974a
Daya Avila :) <43186207+dayaavila@users.noreply.github.com>
2020-06-10 12:37:13 +0200

    2

---
+++ Services/ICityInfoRepository.cs
@@ -0,0 +1,21 @@
+﻿using CityInfoAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoAPI.Services
+{
+    public interface ICityInfoRepository
+    {
+        //IQueryable<City> GetCities();
+
+        //para obtener la ciudad
+        IEnumerable<City> GetCities();
+        City GetCity(int CityId, bool includePointOfInterest);
+
+        //para obtener un punto de interes oara la ciudad
+        IEnumerable<PointOfInterest> GetPointOfInterestForCity(int CityId);
+        PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+    }
+}
